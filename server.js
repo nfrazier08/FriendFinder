6e9d439f8c2020e8aa7bc6a8557453bb6f798699
neliza <nicolasaelf64@gmail.com>
2017-10-18 19:04:12 -0600

    added package.json

---
+++ server.js
@@ -0,0 +1,5 @@
+// server.js file should require the basic npm packages class: `express`, `body-parser` and `path`.
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
