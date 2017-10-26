beaca27c1f45e41ea8e6da94627958a7d36d5b1c
neliza <nicolasaelf64@gmail.com>
2017-10-25 21:11:28 -0600

    finishing touches

--- app/data/clickFunctions.js
+++ app/data/clickFunctions.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
 
-    //jimi photo link: https://www.google.com/imgres?imgurl=http://img.wennermedia.com/920-width/rs-136467-2b1d08ea01d2714a82785574e1434b4dcbecbc53.jpg&imgrefurl=http://www.rollingstone.com/music/lists/100-greatest-guitarists-20111123/jimi-hendrix-20120705&h=920&w=920&tbnid=odgiDuG85b2G1M:&tbnh=186&tbnw=186&usg=__j5KQVBUIjW_aTrviQsmLAFdvXag=&vet=10ahUKEwjp0rjJvYTXAhUojFQKHZv-CM0Q_B0ItgEwFw..i&docid=hW5yY2ziuIEibM&itg=1&sa=X&ved=0ahUKEwjp0rjJvYTXAhUojFQKHZv-CM0Q_B0ItgEwFw#h=920&imgdii=9JbuiEPyfEQkqM:&tbnh=186&tbnw=186&vet=10ahUKEwjp0rjJvYTXAhUojFQKHZv-CM0Q_B0ItgEwFw..i&w=920
-          
+ 
 //Sumbit button click
 $("#submitbtn").on("click", function(){
         event.preventDefault();
@@ -34,11 +33,15 @@ $("#submitbtn").on("click", function(){
             contentType: 'application/json',
             success: function(assignedFriend){
                 console.log(assignedFriend)
+                //modal of assigned friend information, create HTML
+                $('#myModal').modal('toggle')
+                $("#nameOfFriend").html("You're Matched Friend: " + assignedFriend.name);
+                $("#matchedPhoto").attr('src', assignedFriend.photo);
             }
-            
         });
     })
-
+    
+    // $("#friendModal").modal('toggle');
 
 //Click function to go to survey page
 $("#surveybtn").on("click", function(){
